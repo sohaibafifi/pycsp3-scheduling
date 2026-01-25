0db76c7fcd8c9f65da4f2e58b6613b583979de33
Sohaib Afifi <sohaib.lafifi@univ-artois.fr>
2026-01-25 21:06:41 +0100

    chore: bump version to 0.5.0

--- pycsp3_scheduling/__init__.py
+++ pycsp3_scheduling/__init__.py
@@ -22,7 +22,7 @@ This package extends pycsp3 with comprehensive scheduling support including:
 
 from __future__ import annotations
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __author__ = "Sohaib AFIFI"
 
 # Variables
